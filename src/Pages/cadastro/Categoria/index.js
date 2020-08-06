280ab9503e1a8cba5056b74d87495cf38bfa8621
IzabelaUFU <38962331+IzabelaUFU@users.noreply.github.com>
2020-08-06 17:51:47 -0300

    eslint

--- src/Pages/cadastro/Categoria/index.js
+++ src/Pages/cadastro/Categoria/index.js
@@ -1,115 +1,136 @@
-import React, {useState} from 'react';
-import PageDefault from '../../../components/PageDefault';
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 import Button from '../../../components/Button';
 import FormField from '../../../components/FormField';
 
-function CadastroCategoria(){
+function CadastroCategoria() {
     const valoresIniciais = {
         titulo: '',
         descricao: '',
         cor: '',
-    }
+    };
     const [categorias, setCategorias] = useState([]);
     const [valores, setValores] = useState(valoresIniciais);
 
-
-    function setValue(chave, valor){
+    function setValue(chave, valor) {
         setValores({
             ...valores,
-            [chave]:valor,
-        })
+            [chave]: valor,
+        });
     }
 
-    function handleChange(infosDoEvento){
+    function handleChange(infosDoEvento) {
         setValue(
             infosDoEvento.target.getAttribute('name'),
-            infosDoEvento.target.value
+            infosDoEvento.target.value,
         );
     }
 
-    return(
-        <PageDefault>
-            <h1>Cadastro de Categoria: </h1>
+    useEffect(() => {
+        const URL = 'http://localhost:8080/categorias';
+        fetch(URL);
+    }, []);
 
-            <form onSubmit={function handleSubmit(e){
+    return ( <
+        PageDefault >
+        <
+        h1 > Cadastro de Categoria: < /h1>
+
+        <
+        form onSubmit = {
+            function handleSubmit(e) {
                 e.preventDefault();
                 setCategorias([
                     ...categorias,
-                    valores
+                    valores,
                 ]);
 
-                setValores(valoresIniciais)
-            }}>
+                setValores(valoresIniciais);
+            }
+        } >
 
-                <FormField 
-                    label="Nome da Categoria:"
-                    value={valores.titulo}
-                    type="text"
-                    name="titulo"
-                    onChange={handleChange}
-                />
+        <
+        FormField label = "Nome da Categoria:"
+        value = { valores.titulo }
+        type = "text"
+        name = "titulo"
+        onChange = { handleChange }
+        />
 
-                <FormField 
-                    label="Descrição da Categoria:"
-                    value={valores.descricao}
-                    type="textarea"
-                    name="descricao"
-                    onChange={handleChange}
-                />
+        <
+        FormField label = "Descrição da Categoria:"
+        value = { valores.descricao }
+        type = "textarea"
+        name = "descricao"
+        onChange = { handleChange }
+        />
 
-                <FormField 
-                    label="Cor da Categoria:"
-                    value={valores.cor}
-                    type="color"
-                    name="cor"
-                    onChange={handleChange}
-                />
+        <
+        FormField label = "Cor:"
+        value = { valores.cor }
+        type = "color"
+        name = "cor"
+        onChange = { handleChange }
+        />
 
-                {/*
-                <div>
-                    <label>
-                        Nome da Categoria:
-                        <input type="text" name="titulo" value={valores.titulo} onChange={handleChange}/>
-                    </label>
-                </div>
+        {
+            /*
+                    <div>
+                        <label>
+                            Nome da Categoria:
+                            <input type="text" name="titulo" value={valores.titulo} onChange={handleChange}/>
+                        </label>
+                    </div>
 
-                <div>
-                    <label>
-                        Descrição da Categoria:
-                        <textarea type="text" name="descricao" value={valores.descricao} onChange={handleChange}/>
-                    </label>
-                </div>
+                    <div>
+                        <label>
+                            Descrição da Categoria:
+                            <textarea type="text" name="descricao" value={valores.descricao} onChange={handleChange}/>
+                        </label>
+                    </div>
 
-                <div>
-                    <label>
-                        Cor da Categoria:
-                        <input type="color" name="cor" value={valores.cor} onChange={handleChange}/>
-                    </label>
-                </div>
+                    <div>
+                        <label>
+                            Cor da Categoria:
+                            <input type="color" name="cor" value={valores.cor} onChange={handleChange}/>
+                        </label>
+                    </div>
 
-                 */}
+                 */
+        }
 
-                <Button>
-                    Cadastrar
-                </Button>
+        <
+        Button >
+        Cadastrar <
+        /Button>
 
-            </form>
+        <
+        /form>
 
-            <ul>
-                {categorias.map((categoria, indice) => {
-                    return(
-                        <li key={`${categoria}${indice}`}>
-                            {categoria.titulo}
-                        </li>
-                    );
-                })}
-            </ul>
+        {
+            categorias.length === 0 && ( <
+                div >
+                Carregando... <
+                /div>
+            )
+        }
 
-            <Link to="/">
-                Voltar para Home
-            </Link>
-        </PageDefault>
+        <
+        ul > {
+            categorias.map((categoria) => ( <
+                li key = { `${categoria.titulo}` } > { categoria.titulo } <
+                /li>
+            ))
+        } <
+        /ul>
+
+        <
+        Link to = "/" >
+        Voltar para Home <
+        /Link> <
+        /PageDefault>
     );
 }
 export default CadastroCategoria;
--- src/components/Menu/components/ButtonLink/index.js
+++ src/components/Menu/components/ButtonLink/index.js
@@ -1,24 +1,24 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function ButtonLink({ href, className, children }){
-    return(
-        <a href={href} className={className}>
-            {children}
-        </a>
-    );
+function ButtonLink({ href, className, children }) {
+  return (
+    <a href={href} className={className}>
+      {children}
+    </a>
+  );
 }
 
 ButtonLink.defaultProps = {
-    href: '/',
-    className: '',
-    children: '',
+  href: '/',
+  className: '',
 };
 
 ButtonLink.propTypes = {
-    className: PropTypes.string,
-    href: PropTypes.string,
-    children: PropTypes.node.isRequired,
-}
+  className: PropTypes.string,
+  href: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
 
 export default ButtonLink;
