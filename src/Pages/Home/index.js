4c295cc573996b34e04b0b8d68a105fe4a51399b
IzabelaUFU <38962331+IzabelaUFU@users.noreply.github.com>
2020-08-07 19:21:33 -0300

    home/component

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import styled  from 'styled-components';
-import Menu from '../../components/Menu';
-import dadosIniciais from '../../data/dados_iniciais.json';
+/* eslint-disable no-console */
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import BannerMain from '../../components/BannerMain';
 import Carousel from '../../components/Carousel';
+import Menu from '../../components/Menu';
 import Footer from '../../components/Footer';
-
+import categoriasRepository from '../../repositories/categorias';
 
 const AppWrapper = styled.div`
   background: var(--grayDark);
@@ -16,13 +16,55 @@ const AppWrapper = styled.div`
 `;
 
 function Home() {
+  const [dadosIniciais, setDadosIniciais] = useState([]);
+
+  useEffect(() => {
+    categoriasRepository.getAllWithVideos()
+      .then((categoriasComVideos) => {
+        setDadosIniciais(categoriasComVideos);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+  // http://localhost:8080/categorias?_embed=videos
+
   return (
     <AppWrapper>
       <Menu />
+
+      {dadosIniciais.length === 0 && (<div>Carregando...</div>)}
+
+      {dadosIniciais.map((categoria, indice) => {
+        if (indice === 0) {
+          return (
+            <div key={categoria.id}>
+              <BannerMain
+                videoTitle={dadosIniciais[0].videos[0].titulo}
+                url={dadosIniciais[0].videos[0].url}
+                videoDescription={dadosIniciais[0].videos[0].description}
+              />
+              <Carousel
+                ignoreFirstVideo
+                category={dadosIniciais[0]}
+              />
+            </div>
+          );
+        }
+
+        return (
+          <Carousel
+            key={categoria.id}
+            category={categoria}
+          />
+        );
+      })}
+
+      {/*
       <BannerMain
         videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
         url={dadosIniciais.categorias[0].videos[0].url}
-        videoDescription={"O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"}
+        videoDescription="O que é Front-end? Trabalhando na área os termos HTML, CSS e JavaScript fazem parte da rotina das desenvolvedoras e desenvolvedores. Mas o que eles fazem, afinal? Descubra com a Vanessa!"
       />
 
       <Carousel
@@ -36,19 +78,20 @@ function Home() {
 
       <Carousel
         category={dadosIniciais.categorias[2]}
-      />      
+      />
 
       <Carousel
         category={dadosIniciais.categorias[3]}
-      />      
+      />
 
       <Carousel
         category={dadosIniciais.categorias[4]}
-      />      
+      />
 
       <Carousel
         category={dadosIniciais.categorias[5]}
-      />      
+      />
+      */}
 
       <Footer />
     </AppWrapper>
--- src/components/PageDefault/index.js
+++ src/components/PageDefault/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import Menu from '../Menu';
 import Footer from '../Footer';
-import styled from 'styled-components';
 
 const Main = styled.main`
     background-color: var(--black);
@@ -10,17 +12,22 @@ const Main = styled.main`
     padding-top: 50px;
     padding-left: 5%;
     padding-right: 5%;
+    
 `;
 
-function PageDefault({ children }){
-    return(
-        <>
-            <Menu />
-                <Main>
-                    {children}
-                </Main>
-            <Footer />
-        </>
-    );
+function PageDefault({ children }) {
+  return (
+    <>
+      <Menu />
+      <Main>
+        {children}
+      </Main>
+      <Footer />
+    </>
+  );
 }
+PageDefault.propTypes = {
+  children: PropTypes.string.isRequired,
+};
+
 export default PageDefault;
